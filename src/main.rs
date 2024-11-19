0dc41283b2d53ff238fdc4956fbb6309d2e799ad
Federico Dionisi <code@fdionisi.me>
2024-11-19 13:25:51 +0100

    feat: extract user from token

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{sync::Arc, time::Duration};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
 use axum::{
     extract::{Path, State},
@@ -7,7 +7,7 @@ use axum::{
     routing::{delete, get, post},
     Json, Router,
 };
-use axum_auth_provider::{auth_middleware, cached_jwk_set::CachedJwkSet, AuthProvider};
+use axum_auth_provider::{auth_middleware, cached_jwk_set::CachedJwkSet, AuthProvider, Token};
 use clap::Parser;
 use tokio::sync::Mutex;
 use uuid::Uuid;
@@ -51,11 +51,12 @@ pub struct LookupSecret {
 
 #[derive(Clone, Default)]
 struct AppState {
-    keys: Arc<Mutex<Vec<ApiKey>>>,
+    keys: Arc<Mutex<HashMap<String, Vec<ApiKey>>>>,
 }
 
 async fn create_key(
     State(app_state): State<AppState>,
+    token_data: Token,
     Json(key): Json<InputApiKey>,
 ) -> impl IntoResponse {
     let mut keys = app_state.keys.lock().await;
@@ -66,16 +67,23 @@ async fn create_key(
         secret: Uuid::new_v4().to_string(),
     };
 
-    keys.push(api_key.clone());
+    keys.entry(token_data.claims.sub.clone())
+        .or_insert_with(Vec::new)
+        .push(api_key.clone());
 
     Json(api_key).into_response()
 }
 
-async fn list_keys(State(app_state): State<AppState>) -> impl IntoResponse {
-    let keys = app_state.keys.lock().await;
+async fn list_keys(State(app_state): State<AppState>, token_data: Token) -> impl IntoResponse {
+    let mut keys = app_state.keys.lock().await;
+
+    let user_keys = keys
+        .entry(token_data.claims.sub.clone())
+        .or_insert(Vec::new());
 
     Json(
-        keys.iter()
+        user_keys
+            .iter()
             .map(|key| ProtectedApiKey {
                 id: key.id.clone(),
                 name: key.name.clone(),
@@ -85,14 +93,22 @@ async fn list_keys(State(app_state): State<AppState>) -> impl IntoResponse {
     .into_response()
 }
 
-async fn delete_key(State(app_state): State<AppState>, Path(id): Path<Uuid>) -> impl IntoResponse {
+async fn delete_key(
+    State(app_state): State<AppState>,
+    token_data: Token,
+    Path(id): Path<Uuid>,
+) -> impl IntoResponse {
     let mut keys = app_state.keys.lock().await;
 
-    let key_index = keys.iter().position(|key| key.id == id);
+    if let Some(user_keys) = keys.get_mut(&token_data.claims.sub) {
+        let key_index = user_keys.iter().position(|key| key.id == id);
 
-    if let Some(index) = key_index {
-        let _ = keys.remove(index);
-        axum::http::StatusCode::NO_CONTENT.into_response()
+        if let Some(index) = key_index {
+            let _ = user_keys.remove(index);
+            axum::http::StatusCode::NO_CONTENT.into_response()
+        } else {
+            axum::http::StatusCode::NOT_FOUND.into_response()
+        }
     } else {
         axum::http::StatusCode::NOT_FOUND.into_response()
     }
@@ -100,13 +116,18 @@ async fn delete_key(State(app_state): State<AppState>, Path(id): Path<Uuid>) -> 
 
 async fn regenerate_key(
     State(app_state): State<AppState>,
+    token_data: Token,
     Path(id): Path<Uuid>,
 ) -> impl IntoResponse {
     let mut keys = app_state.keys.lock().await;
 
-    if let Some(key) = keys.iter_mut().find(|key| key.id == id) {
-        key.secret = Uuid::new_v4().to_string();
-        Json(key.clone()).into_response()
+    if let Some(user_keys) = keys.get_mut(&token_data.claims.sub) {
+        if let Some(key) = user_keys.iter_mut().find(|key| key.id == id) {
+            key.secret = Uuid::new_v4().to_string();
+            Json(key.clone()).into_response()
+        } else {
+            axum::http::StatusCode::NOT_FOUND.into_response()
+        }
     } else {
         axum::http::StatusCode::NOT_FOUND.into_response()
     }
@@ -114,16 +135,21 @@ async fn regenerate_key(
 
 async fn lookup_key(
     State(app_state): State<AppState>,
+    token_data: Token,
     Json(lookup): Json<LookupSecret>,
 ) -> impl IntoResponse {
     let keys = app_state.keys.lock().await;
 
-    if let Some(key) = keys.iter().find(|key| key.secret == lookup.secret) {
-        Json(ProtectedApiKey {
-            id: key.id,
-            name: key.name.clone(),
-        })
-        .into_response()
+    if let Some(user_keys) = keys.get(&token_data.claims.sub) {
+        if let Some(key) = user_keys.iter().find(|key| key.secret == lookup.secret) {
+            Json(ProtectedApiKey {
+                id: key.id,
+                name: key.name.clone(),
+            })
+            .into_response()
+        } else {
+            axum::http::StatusCode::NOT_FOUND.into_response()
+        }
     } else {
         axum::http::StatusCode::NOT_FOUND.into_response()
     }
@@ -195,11 +221,11 @@ mod tests {
             todo!()
         }
 
-        async fn verify(&self, _: &str) -> Result<TokenData<Claims>, AuthError> {
+        async fn verify(&self, token: &str) -> Result<TokenData<Claims>, AuthError> {
             Ok(TokenData {
                 header: Default::default(),
                 claims: Claims {
-                    sub: "test".to_string(),
+                    sub: token.to_string(),
                     exp: 0,
                 },
             })
@@ -217,40 +243,40 @@ mod tests {
             }
         }
 
-        async fn create_key(&self, key: InputApiKey) -> TestResponse {
+        async fn create_key(&self, key: InputApiKey, token: &str) -> TestResponse {
             self.server
                 .post("/keys")
                 .json(&serde_json::to_value(key).unwrap())
-                .add_header("Authorization", "Bearer test_token")
+                .add_header("Authorization", &format!("Bearer {}", token))
                 .await
         }
 
-        async fn list_keys(&self) -> TestResponse {
+        async fn list_keys(&self, token: &str) -> TestResponse {
             self.server
                 .get("/keys")
-                .add_header("Authorization", "Bearer test_token")
+                .add_header("Authorization", &format!("Bearer {}", token))
                 .await
         }
 
-        async fn delete_key(&self, id: Uuid) -> TestResponse {
+        async fn delete_key(&self, id: Uuid, token: &str) -> TestResponse {
             self.server
                 .delete(&format!("/keys/{}", id))
-                .add_header("Authorization", "Bearer test_token")
+                .add_header("Authorization", &format!("Bearer {}", token))
                 .await
         }
 
-        async fn regenerate_key(&self, id: Uuid) -> TestResponse {
+        async fn regenerate_key(&self, id: Uuid, token: &str) -> TestResponse {
             self.server
                 .post(&format!("/keys/{}", id))
-                .add_header("Authorization", "Bearer test_token")
+                .add_header("Authorization", &format!("Bearer {}", token))
                 .await
         }
 
-        async fn lookup_key(&self, secret: String) -> TestResponse {
+        async fn lookup_key(&self, secret: String, token: &str) -> TestResponse {
             self.server
                 .post("/lookup")
                 .json(&LookupSecret { secret })
-                .add_header("Authorization", "Bearer test_token")
+                .add_header("Authorization", &format!("Bearer {}", token))
                 .await
         }
 
@@ -264,9 +290,12 @@ mod tests {
         let api_key_name = String::from("my api key");
         let client = TestClient::new();
         let response = client
-            .create_key(InputApiKey {
-                name: api_key_name.clone(),
-            })
+            .create_key(
+                InputApiKey {
+                    name: api_key_name.clone(),
+                },
+                "test_token",
+            )
             .await;
 
         assert_eq!(response.status_code(), 200);
@@ -281,19 +310,25 @@ mod tests {
 
         let api_key_name = String::from("my api key 1");
         client
-            .create_key(InputApiKey {
-                name: api_key_name.clone(),
-            })
+            .create_key(
+                InputApiKey {
+                    name: api_key_name.clone(),
+                },
+                "test_token",
+            )
             .await;
 
         let api_key_name = String::from("my api key 2");
         client
-            .create_key(InputApiKey {
-                name: api_key_name.clone(),
-            })
+            .create_key(
+                InputApiKey {
+                    name: api_key_name.clone(),
+                },
+                "test_token",
+            )
             .await;
 
-        let response = client.list_keys().await;
+        let response = client.list_keys("test_token").await;
 
         assert_eq!(response.status_code(), 200);
         assert_eq!(response.json::<Vec<ProtectedApiKey>>().len(), 2);
@@ -305,16 +340,19 @@ mod tests {
 
         let api_key_name = String::from("my api key");
         let response = client
-            .create_key(InputApiKey {
-                name: api_key_name.clone(),
-            })
+            .create_key(
+                InputApiKey {
+                    name: api_key_name.clone(),
+                },
+                "test_token",
+            )
             .await;
         let id = response.json::<ApiKey>().id;
 
-        let delete_response = client.delete_key(id).await;
+        let delete_response = client.delete_key(id, "test_token").await;
         assert_eq!(delete_response.status_code(), 204);
 
-        let list_response = client.list_keys().await;
+        let list_response = client.list_keys("test_token").await;
         assert_eq!(list_response.json::<Vec<ProtectedApiKey>>().len(), 0);
     }
 
@@ -324,13 +362,16 @@ mod tests {
 
         let api_key_name = String::from("my api key");
         let create_response = client
-            .create_key(InputApiKey {
-                name: api_key_name.clone(),
-            })
+            .create_key(
+                InputApiKey {
+                    name: api_key_name.clone(),
+                },
+                "test_token",
+            )
             .await;
         let original_key = create_response.json::<ApiKey>();
 
-        let regenerate_response = client.regenerate_key(original_key.id).await;
+        let regenerate_response = client.regenerate_key(original_key.id, "test_token").await;
         assert_eq!(regenerate_response.status_code(), 200);
 
         let regenerated_key = regenerate_response.json::<ApiKey>();
@@ -345,13 +386,16 @@ mod tests {
 
         let api_key_name = String::from("my api key");
         let create_response = client
-            .create_key(InputApiKey {
-                name: api_key_name.clone(),
-            })
+            .create_key(
+                InputApiKey {
+                    name: api_key_name.clone(),
+                },
+                "test_token",
+            )
             .await;
         let created_key = create_response.json::<ApiKey>();
 
-        let lookup_response = client.lookup_key(created_key.secret).await;
+        let lookup_response = client.lookup_key(created_key.secret, "test_token").await;
         assert_eq!(lookup_response.status_code(), 200);
 
         let looked_up_key = lookup_response.json::<ProtectedApiKey>();
@@ -363,7 +407,9 @@ mod tests {
     async fn test_invalid_lookup_key() {
         let client = TestClient::new();
 
-        let invalid_lookup_response = client.lookup_key("invalid_secret".to_string()).await;
+        let invalid_lookup_response = client
+            .lookup_key("invalid_secret".to_string(), "test_token")
+            .await;
         assert_eq!(invalid_lookup_response.status_code(), 404);
     }
 
@@ -372,5 +418,53 @@ mod tests {
         let client = TestClient::new();
         let response = client.healthz().await;
         assert_eq!(response.status_code(), 200);
+    }
+
+    #[tokio::test]
+    async fn test_different_tokens_access_different_keys() {
+        let client = TestClient::new();
+
+        let user1_key = client
+            .create_key(
+                InputApiKey {
+                    name: "user1_key".to_string(),
+                },
+                "user1_token",
+            )
+            .await
+            .json::<ApiKey>();
+
+        let user2_key = client
+            .create_key(
+                InputApiKey {
+                    name: "user2_key".to_string(),
+                },
+                "user2_token",
+            )
+            .await
+            .json::<ApiKey>();
+
+        let user1_keys = client
+            .list_keys("user1_token")
+            .await
+            .json::<Vec<ProtectedApiKey>>();
+        assert_eq!(user1_keys.len(), 1);
+        assert_eq!(user1_keys[0].id, user1_key.id);
+
+        let user2_keys = client
+            .list_keys("user2_token")
+            .await
+            .json::<Vec<ProtectedApiKey>>();
+        assert_eq!(user2_keys.len(), 1);
+        assert_eq!(user2_keys[0].id, user2_key.id);
+
+        let delete_response = client.delete_key(user2_key.id, "user1_token").await;
+        assert_eq!(delete_response.status_code(), 404);
+
+        let regenerate_response = client.regenerate_key(user1_key.id, "user2_token").await;
+        assert_eq!(regenerate_response.status_code(), 404);
+
+        let lookup_response = client.lookup_key(user2_key.secret, "user1_token").await;
+        assert_eq!(lookup_response.status_code(), 404);
     }
 }
